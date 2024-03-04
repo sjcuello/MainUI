c6053aa3315e3a78faca1270fcb99c17451042eb
Santiago Cuello <sjcuello@gmail.com>
2024-03-04 20:44:24 -0300

    Purge and use components

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,6 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { ThemeContainer } from '@workspaceui/componentlibrary';
-import NewComponent from '@workspaceui/componentlibrary/src/new';
-import { useEffect } from 'react';
+import Home from './screens/Home';
 function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    ThemeContainer('jsjs');
-  }, []);
-  return (
-    <>
-      <NewComponent name="santi" />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
